809b02bec9e5068c9d0438a126362569449d965e
Chris Tulin <47799223+christulin@users.noreply.github.com>
2021-04-23 13:10:44 -0700

    add caching layer in front of SSR app (#4)
    * add caching layer in front of SSR app
    
    * bump up cache time

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ import app from './app.js';
 const PORT = process.env.PORT || 5000;
 import ssr from './ssr.js';
 import newrelic from 'newrelic';
+const cache = require('express-redis-cache')();
 
-app.get('/rooms/:id', ssr);
+app.get('/rooms/:id',
+  (req, res, next) => {
+    const id = req.params.id;
+    if (id) {
+      res.express_redis_cache_name = id;
+    }
+    next();
+  },
+  cache.route({ expire: 50000 }),
+  ssr);
 
 try {
   app.listen(PORT, () => {
